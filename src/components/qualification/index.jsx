91e33f5fef4651c6689a50021f5d4300c9425d62
Thomas Riffard <ferdodo@hotmail.fr>
2018-04-07 10:25:34 +0200

    render experences

--- src/components/qualification/index.jsx
+++ src/components/qualification/index.jsx
@@ -4,13 +4,26 @@ import ReactDOM from 'react-dom';
 export default class Qualification extends React.Component{
 
 
-	renderElement(i) {
+	renderExperience(i){
 		let key = btoa(JSON.stringify(i));
 
 		return (
+			<span key={key}> {i} </span>
+		);
+	}
+
+	renderQualification(i) {
+		let key = btoa(JSON.stringify(i));
+
+		const qualificationStyle = {
+			"margin" : "13mm"
+	    };
+
+		return (
 			
-			<div key={key}>
-				<span> {i.job} </span>
+			<div key={key} style={qualificationStyle}>
+				<div><span><b> {i.job} </b></span></div> 
+				{ i.experiences.map(this.renderExperience) } 
 			</div>
 		);
 	}
@@ -19,7 +32,7 @@ export default class Qualification extends React.Component{
 		
 		return (
 			<div className="Qualification">
-				{ this.props.content.map(this.renderElement) }
+				{ this.props.content.map(this.renderQualification.bind(this)) }
 			</div>
 		);
 	}
