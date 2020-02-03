3646a1edc746c62bcfd3a2e9c38ef72f3d021b5e
Thomas Riffard <ferdodo@hotmail.fr>
2020-02-03 16:31:17 +0100

    Convert space to tabs

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,31 +1,31 @@
 
 module.exports = {
-    "mode" : "production",
+	"mode" : "production",
 	"entry"  : './src/index.jsx',
 	"output" : {
-    	"path"     : __dirname + "/www",
-    	"filename" : "bundle.js"
+		"path"     : __dirname + "/www",
+		"filename" : "bundle.js"
 	},
 	"module" : {
-    	"rules" : [{
-			"test" : /\.css$/,
-			"use"  : ['style-loader', 'css-loader']
-		},{
-			"test"    : /\.jsx$/,
-			"exclude" : /(node_modules|bower_components)/,
-      		"use"     : {
-        		"loader"  : 'babel-loader',
-                "options": {"presets": ['@babel/preset-env', '@babel/preset-react']}
-      		}
-    	},{
-    		"test"   : /\.(png|woff|woff2|eot|ttf|svg)$/,
-    		"loader" : 'url-loader?limit=100000' 
-    	},{
-        	"test" : /\.(png|jpg|gif)$/,
-        	"use"  : [{
-            	"loader": 'file-loader',
-            	"options": {}  
+		"rules" : [{
+				"test" : /\.css$/,
+				"use"  : ['style-loader', 'css-loader']
+			},{
+				"test"    : /\.jsx$/,
+				"exclude" : /(node_modules|bower_components)/,
+				"use"     : {
+					"loader"  : 'babel-loader',
+					"options": {"presets": ['@babel/preset-env', '@babel/preset-react']}
+				}
+			},{
+				"test"   : /\.(png|woff|woff2|eot|ttf|svg)$/,
+				"loader" : 'url-loader?limit=100000' 
+			},{
+				"test" : /\.(png|jpg|gif)$/,
+				"use"  : [{
+					"loader": 'file-loader',
+					"options": {}  
+			}]
 		}]
-      }]
 	}
 };
