005f0c57e61bfa0c5ad243ef034c251a23d1b707
Thomas Riffard <ferdodo@hotmail.fr>
2020-02-16 03:49:05 +0100

    fix file indentation

--- webpack.config.js
+++ webpack.config.js
@@ -10,26 +10,24 @@ module.exports = {
 	"plugins": [new HtmlWebpackPlugin()],
 	"module" : {
 		"rules" : [{
-				"test" : /\.css$/,
-				"use"  : ['style-loader', 'css-loader']
-			},{
-				"test"    : /\.jsx$/,
-				"exclude" : /(node_modules|bower_components)/,
-				"use"     : {
-					"loader"  : 'babel-loader',
-					"options": {"presets": ['@babel/preset-env', '@babel/preset-react']}
-				}
-			},{
-				"test"   : /\.(png|woff|woff2|eot|ttf|svg)$/,
-				"loader" : 'url-loader?limit=100000' 
-			},{
-				"test" : /\.(png|jpg|gif)$/,
-				"use"  : [{
-					"loader": 'file-loader',
-					"options": {}  
-			}]},{
-				"test" : /\.yaml$/,
-				"use" : ['json-loader', 'yaml-loader']
+			"test" : /\.css$/,
+			"use"  : ['style-loader', 'css-loader']
+		},{
+			"test"    : /\.jsx$/,
+			"exclude" : /(node_modules|bower_components)/,
+			"use"     : {
+				"loader"  : 'babel-loader',
+				"options": {"presets": ['@babel/preset-env', '@babel/preset-react']}
+			}
+		},{
+			"test"   : /\.(png|woff|woff2|eot|ttf|svg)$/,
+			"loader" : 'url-loader?limit=100000' 
+		},{
+			"test" : /\.(png|jpg|gif)$/,
+			"use"  : [{"loader": 'file-loader', "options": {}}]
+		},{
+			"test" : /\.yaml$/,
+			"use" : ['json-loader', 'yaml-loader']
 		}]
 	}
 };
